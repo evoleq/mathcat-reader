2dba4d462518dcd36798ef4c1cae713541799971
Florian Schmidt <florian.schmidt@platis.de>
2020-06-15 15:02:06 +0200

    Introduced morphisms

--- src/commonMain/kotlin/org/evoleq/math/cat/monad/reader/reader.kt
+++ src/commonMain/kotlin/org/evoleq/math/cat/monad/reader/reader.kt
@@ -1,13 +1,30 @@
+/**
+ * Copyright (c) 2020 Dr. Florian Schmidt
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.evoleq.math.cat.monad.reader
 
 import org.evoleq.math.cat.marker.MathCatDsl
+import org.evoleq.math.cat.morphism.Morphism
+import org.evoleq.math.cat.morphism.by
 
-interface Reader<E, T> : (E)->T {
+interface Reader<E, T> : Morphism<E, T> {
     infix  fun <S>  map(f: (T)->S): Reader<E, S> = Reader { e ->
-        f(this@Reader(e))
+        f(by(this@Reader)(e))
     }
     @MathCatDsl
-    infix fun runOn(e : E): T = this@Reader(e)
+    infix fun runOn(e : E): T = by(this@Reader)(e)
     
     @MathCatDsl
     infix fun <S> bind(f: (T)-> Reader<E, S>): Reader<E, S> = (this map f).multiply()
@@ -16,15 +33,15 @@ interface Reader<E, T> : (E)->T {
 @MathCatDsl
 @Suppress("FunctionName")
 fun <E, T> Reader(arrow: (E)->T): Reader<E, T> = object : Reader<E, T> {
-    override fun invoke(p1: E): T = arrow(p1)
+    override val morphism: (E) -> T = arrow
 }
 
-interface KlReader<E, K, T> : (K)-> Reader<E, T>
+interface KlReader<E, K, T> : Morphism<K, Reader<E, T>>
 
 @MathCatDsl
 @Suppress("FunctionName")
 fun <E, K, T> KlReader(arrow: (K)-> Reader<E, T>): KlReader<E, K, T> = object : KlReader<E, K, T> {
-    override fun invoke(p1: K): Reader<E, T> = arrow(p1)
+    override val morphism = arrow
 }
 
 @MathCatDsl
@@ -42,5 +59,5 @@ fun <E, T> Reader<E, Reader<E, T>>.multiply(): Reader<E, T> = Reader { e ->
 }
 
 operator fun <E, K, L, M> KlReader<E, K, L>.times(other: KlReader<E, L, M>): KlReader<E, K, M> = KlReader { k ->
-    (this@times(k) map other).multiply()
+    (by(this@times)(k) map by(other)).multiply()
 }

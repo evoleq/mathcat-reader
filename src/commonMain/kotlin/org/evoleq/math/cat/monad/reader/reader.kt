658db48893e45961a5a9d049844f1a3e4c8d3ebc
Florian Schmidt <florian.schmidt@platis.de>
2020-06-23 15:31:12 +0200

    Applicative structure

--- src/commonMain/kotlin/org/evoleq/math/cat/monad/reader/reader.kt
+++ src/commonMain/kotlin/org/evoleq/math/cat/monad/reader/reader.kt
@@ -15,49 +15,106 @@
  */
 package org.evoleq.math.cat.monad.reader
 
+import org.evoleq.math.cat.functor.Diagonal
 import org.evoleq.math.cat.marker.MathCatDsl
 import org.evoleq.math.cat.morphism.Morphism
 import org.evoleq.math.cat.morphism.by
+import org.evoleq.math.cat.morphism.evaluate
+import org.evoleq.math.cat.morphism.o
+import org.evoleq.math.cat.structure.x
 
 interface Reader<E, T> : Morphism<E, T> {
-    infix  fun <S>  map(f: (T)->S): Reader<E, S> = Reader { e ->
-        f(by(this@Reader)(e))
-    }
     @MathCatDsl
     infix fun runOn(e : E): T = by(this@Reader)(e)
-    
-    @MathCatDsl
-    infix fun <S> bind(f: (T)-> Reader<E, S>): Reader<E, S> = (this map f).multiply()
 }
 
+/**
+ * Constructor function for [Reader]
+ */
 @MathCatDsl
 @Suppress("FunctionName")
 fun <E, T> Reader(arrow: (E)->T): Reader<E, T> = object : Reader<E, T> {
     override val morphism: (E) -> T = arrow
 }
 
-interface KlReader<E, K, T> : Morphism<K, Reader<E, T>>
-
-@MathCatDsl
-@Suppress("FunctionName")
-fun <E, K, T> KlReader(arrow: (K)-> Reader<E, T>): KlReader<E, K, T> = object : KlReader<E, K, T> {
-    override val morphism = arrow
+/**********************************************************************************************************************
+ *
+ * Functorial structure
+ *
+ **********************************************************************************************************************/
+/**
+ * Map [Reader]
+ */
+infix  fun <E, S, T>  Reader<E, S>.map(f: (S)->T): Reader<E, T> = Reader { e ->
+    (f o by(this@map)) (e)
 }
+/**********************************************************************************************************************
+ *
+ * Applicative structure
+ *
+ **********************************************************************************************************************/
+/**
+ * Apply function of the Applicative [Reader]
+ */
+fun <E, S, T> Reader<E, (S)->T>.apply(): (Reader<E, S>)->Reader<E, T> = {rS -> Reader{e ->
+    ((by(this@apply) x by(rS)) (Diagonal(e))).evaluate()
+} }
+/**
+ * Apply function of the Applicative [Reader]
+ */
+infix fun <E, S, T> Reader<E, (S)->T>.apply(reader: Reader<E, S>): Reader<E, T> = apply()(reader)
 
+/**********************************************************************************************************************
+ *
+ * Monadic structure
+ *
+ **********************************************************************************************************************/
+/**
+ * Unit of the [Reader] monad
+ */
 @MathCatDsl
 @Suppress("FunctionName")
 fun <E, T> ReturnReader(t : T): Reader<E, T> = Reader { t }
 
-@MathCatDsl
-@Suppress("FunctionName")
-fun <E, T> KlReturnReader(): KlReader<E, T, T> = KlReader { t -> ReturnReader(t) }
-
+/**
+ * Multiplication of the [Reader] mondad
+ */
 @MathCatDsl
 @Suppress("FunctionName")
 fun <E, T> Reader<E, Reader<E, T>>.multiply(): Reader<E, T> = Reader { e ->
     this@multiply runOn e runOn e
 }
 
+/**
+ * Bind function of the [Reader] monad
+ */
+@MathCatDsl
+infix fun <E, S, T> Reader<E, S>.bind(f: (S)-> Reader<E, T>): Reader<E, T> = (this map f).multiply()
+
+/**
+ * Kleisli [Reader]
+ */
+interface KlReader<E, K, T> : Morphism<K, Reader<E, T>>
+
+/**
+ * Constructor function for [KlReader]
+ */
+@MathCatDsl
+@Suppress("FunctionName")
+fun <E, K, T> KlReader(arrow: (K)-> Reader<E, T>): KlReader<E, K, T> = object : KlReader<E, K, T> {
+    override val morphism = arrow
+}
+
+/**
+ * Identity on the [KlReader] monoid
+ */
+@MathCatDsl
+@Suppress("FunctionName")
+fun <E, T> KlReturnReader(): KlReader<E, T, T> = KlReader { t -> ReturnReader(t) }
+
+/**
+ * Multiplication of [KlReader] arrows
+ */
 operator fun <E, K, L, M> KlReader<E, K, L>.times(other: KlReader<E, L, M>): KlReader<E, K, M> = KlReader { k ->
     (by(this@times)(k) map by(other)).multiply()
 }
--- src/commonMain/kotlin/org/evoleq/math/cat/suspend/monad/reader/reader.kt
+++ src/commonMain/kotlin/org/evoleq/math/cat/suspend/monad/reader/reader.kt
@@ -17,19 +17,16 @@ package org.evoleq.math.cat.suspend.monad.reader
 
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.coroutineScope
+import org.evoleq.math.cat.functor.Diagonal
 import org.evoleq.math.cat.marker.MathCatDsl
+import org.evoleq.math.cat.structure.x
 import org.evoleq.math.cat.suspend.morphism.ScopedSuspended
 import org.evoleq.math.cat.suspend.morphism.by
+import org.evoleq.math.cat.suspend.morphism.evaluate
 
 interface Reader<E, T> : ScopedSuspended<E, T> {
-    suspend infix  fun <S>  map(f: suspend CoroutineScope.(T)->S): Reader<E, S> = Reader { e ->
-        f(by(this@Reader)(e))
-    }
     @MathCatDsl
     suspend infix fun runOn(e : E): T = coroutineScope { by(this@Reader)(e) }
-    
-    @MathCatDsl
-    suspend infix fun <S> bind(f: suspend CoroutineScope.(T)-> Reader<E, S>): Reader<E, S> = (this map f).multiply()
 }
 
 @MathCatDsl
@@ -37,29 +34,94 @@ interface Reader<E, T> : ScopedSuspended<E, T> {
 fun <E, T> Reader(arrow: suspend CoroutineScope.(E)->T): Reader<E, T> = object : Reader<E, T> {
     override val morphism: suspend CoroutineScope.(E) -> T = arrow
 }
-
-interface KlReader<E, K, T> : ScopedSuspended<K, Reader<E, T>>
-
-@MathCatDsl
-@Suppress("FunctionName")
-fun <E, K, T> KlReader(arrow: suspend CoroutineScope.(K)-> Reader<E, T>): KlReader<E, K, T> = object : KlReader<E, K, T> {
-    override val morphism: suspend CoroutineScope.(K) -> Reader<E, T> = arrow
+/**********************************************************************************************************************
+ *
+ * Functorial structure
+ *
+ **********************************************************************************************************************/
+/**
+ * Map [Reader]
+ */
+suspend infix fun <E, S, T>  Reader<E, S>.map(f: suspend CoroutineScope.(S)->T): Reader<E, T> = Reader { e ->
+    f(by(this@map)(e))
 }
 
+
+/**********************************************************************************************************************
+ *
+ * Applicative structure
+ *
+ **********************************************************************************************************************/
+/**
+ * Apply function of the applicative [Reader]
+ */
+@MathCatDsl
+suspend fun <E, S, T> Reader<E, suspend CoroutineScope.(S)->T>.apply(): suspend CoroutineScope.(Reader<E, S>)->Reader<E, T> = {
+    reader: Reader<E, S> -> Reader{
+        e -> (by(this@apply) x by(reader)) (Diagonal(e)).evaluate()
+    }
+}
+
+/**
+ * Apply function of the applicative [Reader]
+ */
+@MathCatDsl
+suspend infix fun <E, S, T> Reader<E, suspend CoroutineScope.(S)->T>.apply(reader: Reader<E, S>): Reader<E, T> = coroutineScope {
+    apply()(reader)
+}
+
+/**********************************************************************************************************************
+ *
+ * Monadic structure
+ *
+ **********************************************************************************************************************/
+
+/**
+ * Return the [Reader] monad
+ */
 @MathCatDsl
 @Suppress("FunctionName")
 fun <E, T> ReturnReader(t : T): Reader<E, T> = Reader { t }
 
-@MathCatDsl
-@Suppress("FunctionName")
-fun <E, T> KlReturnReader(): KlReader<E, T, T> = KlReader { t -> ReturnReader(t) }
-
+/**
+ * Multiplication of the [Reader] monad
+ */
 @MathCatDsl
 @Suppress("FunctionName")
 suspend fun <E, T> Reader<E, Reader<E, T>>.multiply(): Reader<E, T> = Reader { e ->
     this@multiply runOn e runOn e
 }
 
+/**
+ * Bind function of the [Reader]
+ */
+@MathCatDsl
+suspend infix fun <E, S, T> Reader<E, S>.bind(f: suspend CoroutineScope.(S)-> Reader<E, T>): Reader<E, T> = (this map f).multiply()
+
+/**
+ * Kleisli [Reader]
+ */
+interface KlReader<E, K, T> : ScopedSuspended<K, Reader<E, T>>
+
+/**
+ * Constructor function for the [KlReader]
+ */
+@MathCatDsl
+@Suppress("FunctionName")
+fun <E, K, T> KlReader(arrow: suspend CoroutineScope.(K)-> Reader<E, T>): KlReader<E, K, T> = object : KlReader<E, K, T> {
+    override val morphism: suspend CoroutineScope.(K) -> Reader<E, T> = arrow
+}
+
+/**
+ * Identity element of the [Reader] monad
+ */
+@MathCatDsl
+@Suppress("FunctionName")
+fun <E, T> KlReturnReader(): KlReader<E, T, T> = KlReader { t -> ReturnReader(t) }
+
+/**
+ * Multiplication of [KlReader]s
+ */
 suspend operator fun <E, K, L, M> KlReader<E, K, L>.times(other: KlReader<E, L, M>): KlReader<E, K, M> = KlReader { k ->
     (by(this@times)(k) map by(other)).multiply()
 }
